b4669cec75f7c5f057cd72a750fa79c23c5f33ac
Majid2851 <majid.bagheri@hssoft.com>
2025-11-18 21:44:53 +0330

    Simple register and login are created.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from .forms import RegisterUserForm
+from django.contrib.auth import login
 
-# Create your views here.
+class Login(LoginView):
+    template_name = "users/accounts/login.html"
+
+class RegisterUser(FormView):
+    template_name = "users/accounts/register.html"
+    form_class = RegisterUserForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+
