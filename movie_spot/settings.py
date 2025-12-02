6b35cbc9233f7f2fc47cca6d6c0044fc03fe6c81
Majid2851 <majid.bagheri@hssoft.com>
2025-12-02 21:52:15 +0330

    Forgetting password is added.

--- movie_spot/settings.py
+++ movie_spot/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.CustomUser"
+
+# Email settings (development - prints to console)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,33 @@
 from django.urls import path
-from .views import Login
-from .views import RegisterUser
+from django.contrib.auth import views as auth_views
+from .views import Login, RegisterUser
 
-urlpatterns =[
-    path("login/",Login.as_view(), name="login"),
-    path("register/",RegisterUser.as_view() ,name="register")
+urlpatterns = [
+    path("login/", Login.as_view(), name="login"),
+    path("register/", RegisterUser.as_view(), name="register"),
+
+    # Password Reset URLs
+    path("password-reset/",
+         auth_views.PasswordResetView.as_view(
+             template_name="users/accounts/password_reset.html"
+         ),
+         name="password_reset"),
+
+    path("password-reset/done/",
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="users/accounts/password_reset_done.html"
+         ),
+         name="password_reset_done"),
+
+    path("password-reset-confirm/<uidb64>/<token>/",
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="users/accounts/password_reset_confirm.html"
+         ),
+         name="password_reset_confirm"),
+
+    path("password-reset-complete/",
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="users/accounts/password_reset_complete.html"
+         ),
+         name="password_reset_complete"),
 ]
